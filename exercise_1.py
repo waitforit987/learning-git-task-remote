dbfbc10016b93a8160a801ea35a2b9e6c3713b28
Waitforit987 <maciejdudzik90@gmail.com>
2024-01-22 22:22:00 +0100

    printing dict and sum

--- exercise_1.py
+++ exercise_1.py
@@ -6,3 +6,8 @@ shopping_dict = {
 for keys, values in shopping_dict.items():
   for i in range (len(values)):
     values[i] = values[i].capitalize()
+  print(f"Idę do {keys.capitalize()} kupuję tu następujące rzeczy {values}")
+
+sum = len(shopping_dict["bakery"]) + len(shopping_dict["grocery"])
+
+print(f"Suma to {sum}")
