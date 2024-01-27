baf943031354d256e857feb3d65c1230d2c9d782
Maciej Dudzik <maciejdudzik90@gmail.com>
2024-01-27 11:18:03 +0100

    adding palindrome file

---
+++ greetings.py
@@ -0,0 +1,42 @@
+# def shopping():
+#     shopping_items = [
+#         "jajka",
+#         "bułka",
+#         "ser feta",
+#         "masło",
+#         "pomidor"
+#     ]
+#     shopping_cart = "Koszyk zawiera: "
+#     for item in shopping_items:
+#         shopping_cart += item + '\n'
+#     return shopping_cart
+
+# print(shopping())
+
+# def no_result_function():
+#     print("I am just printing I love you")
+#     print("and returning nothing to you!")
+#     print(type(None))
+
+# no_result_function()
+
+# def customized_hello(first_name, last_name, gender_prefix='Mr'):
+#     print(f"Hello {gender_prefix} {first_name} {last_name}!")
+
+# customized_hello("John", "Cleese")
+# customized_hello("Clara", "Cleese", "Ms")
+
+shopping_items = [
+    "jajka",
+    "bułka",
+    "ser feta",
+    "masło",
+    "pomidor"
+]
+
+def shopping(items, payment='card', shop='local'):
+    print("test")
+    pass
+
+
+shopping(shopping_items, payment="cash")
